e57a4e4521b1796128882f692ef2bc4858aa7e9e
Taras <taras_ilyashchuk161@ukr.net>
2020-06-04 01:17:29 +0300

    Task form

--- frontend/src/components/layout/MainBody.js
+++ frontend/src/components/layout/MainBody.js
@@ -8,6 +8,15 @@ export class MainBody extends Component{
       loaded: false,
       placeholder: "Loading"
     };
+     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+    handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+    handleSubmit(event) {
+        alert(this.state.data);
+        event.preventDefault();
   }
     componentDidMount() {
     fetch("api/task")
@@ -23,22 +32,52 @@ export class MainBody extends Component{
         this.setState(() => {
           return {
             data,
-            loaded: true
+            loaded: true,
+              formFields:data.fields
           };
         });
       });
   }
     render(){
-        return (
-            <div>
-                {this.state.data.map(user => {
-                    return (
-                    <p>
-                        {user.title}
-                    </p>)
-                })
-                }
 
+
+        return (
+            <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-6">
+                    <h3>Adding new Task</h3>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Title:
+                            <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+
+
+
+                        <input type="submit" value="Надіслати" />
+                    </form>
+                </div>
+                <div className="col-sm-6">
+                    <div>
+                        <ul className="list-group ">
+                            <li className="list-group-item justify-content-center align-items-center d-flex">
+                                Add new task
+                            </li>
+                            {this.state.data.map(task => {
+                    return (
+
+                            <li className="list-group-item justify-content-center align-items-center d-flex">
+                                {task.title}
+                            </li>
+
+                    )
+
+                })}
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
             </div>
         )
     }
